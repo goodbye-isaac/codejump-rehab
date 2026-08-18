a80e815c784a037c2af41afc98e125585be5b1c9
goodbye-isaac <goodbyeisaac1727@gmail.com>
2026-08-18 15:10:45 +0900

    feat: ハンバーガーメニュー内リンククリック時にメニューを閉じる処理を追加

--- lp-vanilla/js/main.js
+++ lp-vanilla/js/main.js
@@ -2,7 +2,15 @@ const hamburger = document.getElementById('js-hamburger');
 const nav = document.getElementById('js-nav');
 
 hamburger.addEventListener('click',() => {
-// is-activeクラスをつけ外しする
+// is-openクラスをつけ外しする
 	hamburger.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
 })
+
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(navLink => {
+	navLink.addEventListener('click',()=> {
+		hamburger.classList.remove('is-open');
+		nav.classList.remove('is-open');		
+	})
+});
